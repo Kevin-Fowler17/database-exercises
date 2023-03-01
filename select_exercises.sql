e83b84261c233a15dc075e637e43edb7a3a11f02
Kevin Fowler <kevin.m.fowler17@gmail.com>
2023-03-01 16:25:10 -0600

    mysql basic statements exercise done

--- select_exercises.sql
+++ select_exercises.sql
@@ -2,10 +2,10 @@ SELECT 'The name of all albums by Pink Floyd.' AS 'Info';
 SELECT album_name FROM albums WHERE artist = 'Pink Floyd';
 
 SELECT 'The year Sgt. Pepper''s Lonely Hearts Club Band was released' AS 'Info';
-SELECT release_date FROM albums WHERE id = 28;
+SELECT release_date FROM albums WHERE album_name = "Sgt. Pepper's Lonely Hearts Club Band";
 
 SELECT 'The genre for Sgt. Pepper''s Lonely Hearts Club Band' AS 'Info';
-SELECT genre FROM albums WHERE id = 28;
+SELECT genre FROM albums WHERE album_name = "Sgt. Pepper's Lonely Hearts Club Band";
 
 SELECT 'Albums were released in the 1990s' AS 'Info';
 SELECT release_date, album_name FROM albums WHERE release_date BETWEEN 1990 AND 1999;
