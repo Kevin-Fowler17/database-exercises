709274383618aa4eee4193eb2ed14e9bbd5ae8ae
Kevin Fowler <kevin.m.fowler17@gmail.com>
2023-03-02 13:28:46 -0600

    mysql limit exercise done

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -31,3 +31,8 @@ SELECT *
 FROM employees
 WHERE last_name LIKE 'E%' or last_name LIKE '%E'
 ORDER BY emp_no;
+
+SELECT *
+FROM employees
+WHERE last_name LIKE 'E%' or last_name LIKE '%E'
+ORDER BY emp_no DESC ;
