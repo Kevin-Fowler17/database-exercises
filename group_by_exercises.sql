1aa34ab70d30fd59e3ed9768b9ebdf91d374325a
Kevin Fowler <kevin.m.fowler17@gmail.com>
2023-03-02 15:24:55 -0600

    mysql group by exercise done

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -1,18 +1,33 @@
+USE ymir_employees;
+
 SELECT COUNT(*) FROM employees;
 
+SELECT DISTINCT title FROM titles;
 
-SELECT DISTINCT
+SELECT DISTINCT last_name
 FROM employees
-WHERE first_name NOT LIKE '%a%';
+WHERE last_name LIKE 'E%E'
+ORDER BY last_name;
 
-SELECT first_name, COUNT(first_name)
+SELECT DISTINCT first_name, last_name
 FROM employees
-WHERE first_name NOT LIKE '%a%'
-GROUP BY first_name;
+WHERE last_name LIKE 'E%E'
+ORDER BY last_name;
 
-SELECT hire_date, COUNT(*)
+SELECT DISTINCT last_name
 FROM employees
-GROUP BY hire_date
-ORDER BY COUNT(*) DESC
-LIMIT 10;
+WHERE last_name LIKE '%q%' and not last_name LIKE '%qu%';
+
+# DISTINCT is redundant with GROUP BY
+# SELECT DISTINCT COUNT(last_name), last_name
+SELECT COUNT(last_name), last_name
+FROM employees
+WHERE last_name LIKE '%q%' and not last_name LIKE '%qu%'
+GROUP BY last_name;
+
+# SELECT DISTINCT COUNT(last_name), last_name
+SELECT COUNT(gender), gender
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+GROUP BY gender;
 
