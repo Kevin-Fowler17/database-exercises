0f833be8ca816ccbf63e0596e34d1c8690f7c847
Kevin Fowler <kevin.m.fowler17@gmail.com>
2023-03-02 15:46:58 -0600

    mysql aliases exercise done

---
+++ aliases_exercises.sql
@@ -0,0 +1,39 @@
+USE ymir_employees;
+
+SELECT CONCAT(emp_no, ' - ', last_name, ', ', first_name) AS full_name, birth_date AS DOB
+FROM employees
+LIMIT 10;
+
+
+# SELECT CONCAT(first_name, ' ', last_name) AS full_name
+# FROM employees
+# LIMIT 25;
+#
+# SELECT CONCAT(first_name, ' ', last_name) AS full_name
+# FROM employees
+# ORDER BY full_name
+# LIMIT 25;
+#
+# SELECT CONCAT(first_name, ' ', last_name) AS full_name
+# FROM employees
+# GROUP BY full_name, last_name
+# ORDER BY last_name
+# LIMIT 25;
+#
+# SELECT hire_date, COUNT(*)
+# FROM employees
+# GROUP BY hire_date
+# ORDER BY COUNT(*) DESC
+# LIMIT 10;
+#
+# SELECT hire_date, COUNT(*) as number_hired
+# FROM employees
+# GROUP BY hire_date
+# ORDER BY number_hired DESC
+# LIMIT 10;
+#
+# SELECT CONCAT(first_name, ' ', last_name) AS full_name
+# FROM employees AS emp
+# GROUP BY full_name, last_name
+# ORDER BY last_name
+# LIMIT 25;
