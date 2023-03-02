2ba7c8332e9f08ddeaa0c48419575d37d1f777c9
Kevin Fowler <kevin.m.fowler17@gmail.com>
2023-03-02 15:34:57 -0600

    mysql group by exercise done

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -14,16 +14,18 @@ FROM employees
 WHERE last_name LIKE 'E%E'
 ORDER BY last_name;
 
-SELECT DISTINCT last_name
+SELECT last_name
 FROM employees
-WHERE last_name LIKE '%q%' and not last_name LIKE '%qu%';
+WHERE last_name LIKE '%q%' and not last_name LIKE '%qu%'
+GROUP BY last_name;
 
 # DISTINCT is redundant with GROUP BY
 # SELECT DISTINCT COUNT(last_name), last_name
 SELECT COUNT(last_name), last_name
 FROM employees
 WHERE last_name LIKE '%q%' and not last_name LIKE '%qu%'
-GROUP BY last_name;
+GROUP BY last_name
+ORDER BY last_name;
 
 # SELECT DISTINCT COUNT(last_name), last_name
 SELECT COUNT(gender), gender
