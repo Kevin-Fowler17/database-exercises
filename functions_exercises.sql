881258ff694c60e1612166d9826eff60ffe37af4
Kevin Fowler <kevin.m.fowler17@gmail.com>
2023-03-02 14:44:16 -0600

    mysql function exercise done

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -1,29 +1,9 @@
 USE ymir_employees;
 
-SELECT emp_no, first_name, last_name
-FROM employees
-WHERE (first_name = 'Irena' or first_name = 'Vidya' or first_name = 'Maya')
-ORDER BY first_name;
-
-SELECT emp_no, first_name, last_name
-FROM employees
-WHERE (first_name = 'Irena' or first_name = 'Vidya' or first_name = 'Maya')
-ORDER BY first_name, last_name;
-
-SELECT emp_no, first_name, last_name
-FROM employees
-WHERE (first_name = 'Irena' or first_name = 'Vidya' or first_name = 'Maya')
-ORDER BY last_name, first_name;
-
-SELECT CONCAT(first_name, ' ', last_name)
+SELECT CONCAT(first_name, ' ', last_name) AS full_name
 FROM employees
 WHERE last_name LIKE 'E%' AND last_name LIKE '%E'
 ORDER BY emp_no;
-
-SELECT *
-FROM employees
-WHERE last_name LIKE 'E%' or last_name LIKE '%E'
-ORDER BY emp_no DESC ;
 
 SELECT *
 FROM employees
