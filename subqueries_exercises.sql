632b0984401127e7d25be177c045443e53b40563
Kevin Fowler <kevin.m.fowler17@gmail.com>
2023-03-03 16:25:27 -0600

    mysql subqueries exercise

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -1,12 +1,44 @@
 USE ymir_employees;
 
-SELECT first_name, last_name, birth_date
-FROM employees
-WHERE emp_no IN (
-    SELECT emp_no
+SELECT *
+FROM employees as e
+WHERE e.hire_date IN (
+    SELECT e2.hire_date
+    FROM employees as e2
+    where e2.emp_no = 101010);
+
+SELECT e.first_name, t.title
+FROM employees as e
+    INNER JOIN titles as t on e.emp_no = t.emp_no
+WHERE first_name IN (
+    SELECT e2.first_name
+    FROM employees as e2
+    where e2.first_name = 'Aamod');
+
+SELECT t.title, COUNT(t.title)
+FROM employees as e
+         INNER JOIN titles as t on e.emp_no = t.emp_no
+WHERE first_name IN (
+    SELECT e2.first_name
+    FROM employees as e2
+    where e2.first_name = 'Aamod')
+GROUP BY (title);
+
+SELECT e.first_name, e.last_name
+FROM employees as e
+WHERE e.gender = 'f' AND e.emp_no IN (
+    SELECT dm.emp_no FROM dept_manager as dm
+    WHERE year(dm.to_date) = 9999);
+
+SELECT dept_name
+FROM departments
+WHERE dept_no IN (
+    SELECT dept_no
     FROM dept_manager
-)
-LIMIT 10;
+    WHERE to_date LIKE '9%' AND emp_no IN (
+        SELECT emp_no
+        FROM employees
+        WHERE gender = 'f'));
 
 
 # SELECT first_name, last_name, birth_date
@@ -16,7 +48,7 @@ LIMIT 10;
 #     FROM dept_manager
 # )
 # LIMIT 10;
-
+#
 # USE ymir_kevin;
 #
 # INSERT INTO users (name, email, role_id)
